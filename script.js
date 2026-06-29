fe2adbd0bd807c2160390397fd830293b7aaff48
anmutDev <anmutadmase@gmail.com>
2026-06-29 06:23:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -435,16 +435,18 @@ function showSection(sectionId, clickedItem){
 // ======================
 // INITIALIZE
 // ======================
-document.addEventListener('click',e=>{
-  const menu=document.getElementById('sidebar');
-  const btn=document.getElementById('menuToggle');
+//outside click
+document.addEventListener("click", (e) => {
 
-  if(menu.classList.contains('open') &&
-     !menu.contains(e.target) &&
-     !btn.contains(e.target)){
+    if (
+        window.innerWidth <= 900 &&
+        sidebar.classList.contains("show") &&
+        !sidebar.contains(e.target) &&
+        !menuToggle.contains(e.target)
+    ) {
+        sidebar.classList.remove("show");
+    }
 
-    menu.classList.remove('open');
-  }
 });
 
 loadData();
